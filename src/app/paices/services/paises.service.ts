4c039f394ab26da10a9bb80ec12aa68b5b6992eb
Lautaro D Alvarez <lautarodalvarez@gmail.com>
2023-04-20 17:14:43 -0300

    se finalizo la app

--- src/app/paices/services/paises.service.ts
+++ src/app/paices/services/paises.service.ts
@@ -32,14 +32,28 @@ export class PaisesService {
     return new HttpParams().set( 'fields', 'name,flags,population,capital,cca2' ) 
   }
   
-  constructor( private http: HttpClient ) { }
+  constructor( private http: HttpClient ) { 
+    this.loadCache();
+  }
+
+  saveCachE(){
+    localStorage.setItem('cacheStorage', JSON.stringify( this.cacheStore ))
+  }  
+
+  loadCache(){
+    if( !localStorage.getItem('cacheStorage'))return;
   
+    this.cacheStore = JSON.parse( localStorage.getItem('cacheStorage')! )
+  }
+
+
   buscarPais( termino: string): Observable<Countries[]> {
     
     const url = `${ this._apiUrl }/name/${termino}`;
     return this.http.get<Countries[]>( url, {params: this.httpParams} )
                     .pipe(
-                      tap( countries => this.cacheStore.byCountries = {term: termino, countries } )
+                      tap( countries => this.cacheStore.byCountries = {term: termino, countries } ),
+                      tap( () => this.saveCachE() )
                     );
   }
   buscarCapital( termino: string): Observable<Countries[]> {
@@ -47,7 +61,8 @@ export class PaisesService {
     const url = `${ this._apiUrl }/capital/${termino}`;
     return this.http.get<Countries[]>( url, {params: this.httpParams} )
                     .pipe(
-                      tap( countries => this.cacheStore.byCapital = { term: termino, countries } )
+                      tap( countries => this.cacheStore.byCapital = { term: termino, countries } ),
+                      tap( () => this.saveCachE() )
                     );
   }
   bucarPaisCodigo( id: string ): Observable<Countries[]>{
@@ -60,7 +75,8 @@ export class PaisesService {
     const url = `${ this._apiUrl }/region/${region}`;
     return this.http.get<Countries[]>( url, {params: this.httpParams} )
                     .pipe(
-                      tap( countries => this.cacheStore.byRegion = { region, countries } )
+                      tap( countries => this.cacheStore.byRegion = { region, countries } ),
+                      tap( () => this.saveCachE() )
                     );
   }
 }
